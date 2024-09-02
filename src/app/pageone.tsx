644efeeb7e84228dec35859ef5e69e31ccc03f56
Kingsley Tagbo <kingsleytagbo@gmail.com>
2024-09-02 17:23:35 -0500

    landing page one

---
+++ src/app/pageone.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageOne = () => {
+  return (
+    <div>
+      <h1>Welcome to Page One</h1>
+      <p>This is the content of Page One.</p>
+    </div>
+  );
+};
+
+export default PageOne;
